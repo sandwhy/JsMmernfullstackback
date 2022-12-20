4e71e0cc075a8f5bd365c0e65d5e555d7705be8d
sandwhy <83949640+sandwhy@users.noreply.github.com>
2022-12-20 14:35:43 +0700

    fixed something

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 
-app.use("/posts", postRoutes)
-app.use("/user", userRoutes)
-
 app.get("/", (req,res) => {
     res.send("APP IS RUNNING")
 })
+app.use("/posts", postRoutes)
+app.use("/user", userRoutes)
+
 
 const PORT = process.env.PORT
 
